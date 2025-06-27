997e23e8512ef25a0c7b9c1f02d9feb36ae039ca
rano <0x1feb@gmail.com>
2025-06-27 19:00:52 +0900

    fix failed test

--- src/test/kotlin/com/example/workflow/WorkflowApplicationTests.kt
+++ src/test/kotlin/com/example/workflow/WorkflowApplicationTests.kt
@@ -3,7 +3,6 @@ package com.example.workflow
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class WorkflowApplicationTests {
 
     @Test
