8e180f87798a89f06b40f1f22f7053fd908f9b37
r4no <43287732+0x1feb@users.noreply.github.com>
2025-06-27 20:04:47 +0900

    Develop (#8)
    * fix failed test (#2)
    
    * Feature/test (#5)
    
    * fix failed test
    
    * fix test2
    
    * Create gradle.yml
    
    * Update gradle.yml
    
    * Update gradle.yml (#1)
    
    * fix test3

--- src/test/kotlin/com/example/workflow/WorkflowApplicationTests.kt
+++ src/test/kotlin/com/example/workflow/WorkflowApplicationTests.kt
@@ -1,12 +1,8 @@
 package com.example.workflow
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
 class WorkflowApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
-
+    @Test fun contextLoads() {}
 }
